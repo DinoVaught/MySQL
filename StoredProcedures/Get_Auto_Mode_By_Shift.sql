c31425c0bb699e8aef2b44b92cc719c5f7450d89
DinoVaught <dinovaught@gmail.com>
2022-08-22 11:09:55 -0500

    new Stored Procedures

--- StoredProcedures/Get_Auto_Mode_By_Shift.sql
+++ StoredProcedures/Get_Auto_Mode_By_Shift.sql
@@ -11,8 +11,24 @@ BEGIN
     SET start_date = CONCAT(SUBDATE(qryDate, INTERVAL 1 DAY), ' 23:00');
     SET end_date = CONCAT(qryDate, ' 22:59');
 
-    CALL debug_msg_log('****', start_date, end_date);
+    -- CALL debug_msg_log('****', start_date, end_date);
+
+	SELECT CASE WHEN HOUR(created) IN(0, 1, 2, 3, 4, 5, 6, 23) THEN '3'
+				WHEN HOUR(created) IN(7, 8, 9, 10, 11, 12, 13, 14) THEN '1'
+				WHEN HOUR(created) IN(15, 16, 17, 18, 19, 20, 21, 22) THEN '2'
+		   END AS 'Shift',
+			val_1 as 'Machine',
+		   TRUNCATE(SUM(TIMESTAMPDIFF(SECOND, created, date_1) / 60), 2)  as 'Minutes',
+		   TRUNCATE(SUM(TIMESTAMPDIFF(SECOND, created, date_1) / 60 / 60), 2)  as 'Hours'
+	FROM   logs_data
+	WHERE  rec_type = 'Auto Mode Gages'
+	AND    date_1 IS NOT NULL
+	AND    created >= start_date  -- '2022-08-20 23:00' -- 11:00 PM
+	AND    created <= end_date -- '2022-08-21 22:59' -- 10:59 PM 
+	GROUP BY  val_1, Shift
+	ORDER  BY val_1, created;
 
 
-	END;
+
+END;
 
